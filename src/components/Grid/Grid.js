9923420afcc385994b236b59c4f1b22b8392c264
sandeepramachandra <sandeepap11@gmail.com>
2021-05-15 20:03:19 +0530

    fix: table and css changes

--- src/components/Grid/Grid.js
+++ src/components/Grid/Grid.js
@@ -11,30 +11,30 @@ const Grid = ({ matches, rowsPerPage }) => {
   }
 
   return (
-    <div className="grid-container">
-      <div>
-        <div className="grid-header">
-          <div className="grid-row-gamenumber">NO.</div>
-          <div className="grid-row-datetime">DATE</div>
-          <div className="grid-row-stage">STAGE</div>
-          <div className="grid-row-location">STADIUM</div>
-          <div className="grid-row-venue">CITY</div>
-          <div className="grid-row-home">TEAM 1</div>
-          <div className="grid-row-away">TEAM 2</div>
-          <div className="grid-row-score">SCORE</div>
-          <div className="grid-row-attendance">ATTENDANCE</div>
-        </div>
-      </div>
-      <div className="grid-body">
-        {matches.map(match => (
+    <table className="grid-container">
+      <thead>
+        <tr>
+          <th>NO.</th>
+          <th>DATE</th>
+          <th>STAGE</th>
+          <th>STADIUM</th>
+          <th>CITY</th>
+          <th>TEAM 1</th>
+          <th>TEAM 2</th>
+          <th>SCORE</th>
+          <th>ATTENDANCE</th>
+        </tr>
+      </thead>
+      <tbody>
+        {matches.map((match) => (
           <GridRow key={match.fifa_id} match={match} />
         ))}
         {rowsPerPage > matches.length &&
-          emptyRows.map(emptyRow => (
+          emptyRows.map((emptyRow) => (
             <div key={emptyRow} className="grid-row-empty"></div>
           ))}
-      </div>
-    </div>
+      </tbody>
+    </table>
   );
 };
 
